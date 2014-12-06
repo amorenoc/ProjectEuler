63c68d4ea6e9ae941a883437829291f8ee0b2e18
Alexandre Moreno <alex@Alexandres-MacBook-Pro.local>
2014-12-06 16:18:06 +0800

    p15

--- p15.py
+++ p15.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 
 def p15(n):
    """Prints out n rows of Pascal's triangle.
@@ -11,47 +10,3 @@ def p15(n):
    return row[len(row)/2]
 
 print p15(20)
-=======
-from collections import deque
-
-result = 0
-result2 = 0
-
-def foo(side_len):
-    global result
-    x = deque([(0,0)])
-    while len(x):
-        (a,b) = x.popleft()
-
-        if a == side_len and b == side_len:
-            result += 1
-
-        if a < side_len:
-            x.append((a+1,b))
-
-        if b < side_len:
-            x.append((a,b+1))
-
-def bar(side_len):
-    global result2
-    x = deque([(0,0,1)])
-    while len(x):
-        (a,b,c) = x.popleft()
-
-        if a == side_len and b == side_len:
-            result += 1
-
-        if a < side_len:
-            [ x[(r,s,t)] += x[(a,b,c)] for r,s,t in x if r == a+1 and s == b]
-            if  x.append(a+1,b,c) 
-
-        if b < side_len:
-            if (a,b+1) in x:
-                x[(a,b+1)] += x[(a,b)]
-            x.append((a,b+1))
-
-foo(3)
-bar(2)
-
-print result
->>>>>>> 3f8a484b1f456290be1fd7955fae8f0c8ca4b69c
